8242b134f369ae2ccac94783b19e92c7145f5bb4
Draku Quay <vantayden@gmail.com>
2021-04-13 10:53:38 +0700

    update embed v2

--- example/android/app/src/main/java/com/wemap/wemapgl_example/MainActivity.java
+++ example/android/app/src/main/java/com/wemap/wemapgl_example/MainActivity.java
@@ -1,15 +1,7 @@
 package asia.wemap.wemapgl.test;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
